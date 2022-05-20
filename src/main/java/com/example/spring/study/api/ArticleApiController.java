dfdc9dc1a2b9840ad61fe9d17032d91d75b989a4
kimdongwoo <kimdongwoo0930@gmail.com>
2022-05-20 18:05:32 +0900

    서비스 계층과 트랜잭션을 이용해 역할분담

--- src/main/java/com/example/spring/study/api/ArticleApiController.java
+++ src/main/java/com/example/spring/study/api/ArticleApiController.java
@@ -4,6 +4,8 @@ package com.example.spring.study.api;
 import com.example.spring.study.dto.ArticleForm;
 import com.example.spring.study.entity.Article;
 import com.example.spring.study.repository.ArticleRepository;
+import com.example.spring.study.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,62 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
-    @Autowired
-    private ArticleRepository articleRepository;
+    @Autowired  // DI, 생성 객체를 가져와 연결
+    private ArticleService articleService;
 
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index(){
+        return articleService.index();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id){
+        return articleService.show(id);
+    }
+
+    // POST
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> create(@RequestBody ArticleForm Dto){  //Request 바디에서 가져와라
+        Article created = articleService.create(Dto);
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created)
+                : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+
+    //PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@RequestBody ArticleForm dto, @PathVariable Long id){
+        Article updated = articleService.update(id,dto);
+        return (updated != null) ? ResponseEntity.status(HttpStatus.OK).body(updated):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    //DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id){
+        Article deleted = articleService.delete(id);
+        //데이터 반환
+        return (deleted != null) ? ResponseEntity.status(HttpStatus.OK).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    // 트랜잭션 -> 실패 -> 롤백
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos) {
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+
+
+    /*
     // GET
     @GetMapping("/api/articles")
     public List<Article> index(){
@@ -67,4 +120,6 @@ public class ArticleApiController {
         //데이터 반환
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+    */
+
 }
